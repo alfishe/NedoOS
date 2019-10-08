acf1d494d6de14b3df795f33a02fc760c586d961
DimkaM <dimkam@users.noreply.github.com>
2019-10-08 11:58:42 +0000

    пофиксил setpath.bat

--- src/_sdk/setpath.bat
+++ src/_sdk/setpath.bat
@@ -1,7 +1,7 @@
-if "%settedpath%"=="" (
-	set settedpath=1
-	path=%PATH%;%~dp0../../tools/;%~dp0;%~dp0../../us/
-)
+if NOT "%settedpath%"=="" exit /b
+set settedpath=1
+set path=%~dp0../../tools/;%~dp0;%~dp0../../us/;%PATH%
+
 rem if "%settedpath%"=="" set sjasmplus="%~dp0/../../tools/sjasmplus"
 rem if "%mhmt%"=="" set mhmt="%~dp0/../../tools/mhmt"
 rem if "%nedores%"=="" set nedores="%~dp0/nedores"
