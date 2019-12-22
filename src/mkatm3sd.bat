64971604c6d3dd7c041280ce569e2213d863bfde
Alone <alone@users.noreply.github.com>
2019-12-22 11:37:58 +0000

    r312: (no commit message)

--- src/mkatm3sd.bat
+++ src/mkatm3sd.bat
@@ -9,4 +9,4 @@ call make.bat noneedtrd
 ..\tools\sjasmplus --nologo kernel\hobeta.asm > nul
 move /Y nedoos.$C ..\release\osatm3sd.$C > nul
 call ..\tools\chkimg.bat sd
-if "%makeall%"=="" ..\us\emul.exe -i ..\us\dimkam.ini
+if "%makeall%"=="" ..\us\emul.exe
