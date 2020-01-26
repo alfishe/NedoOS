dd0b5fc701722625c7cd9c4dcd763edc83aa6896
Alone <alone@users.noreply.github.com>
2020-01-26 19:41:19 +0000

    -NGS detect fixed

--- src/kernel/ngssddrv.asm
+++ src/kernel/ngssddrv.asm
@@ -71,6 +71,7 @@ SDWRSN2		OUTI
 
 ;ЧТЕНИЕ "A" СЕКТОРОВ
 ;SDRDMUL		EX AF,AF'
+	display "readsectorsGS=",$
 readsectorsGS
 		LD A,_DEV_READ
 		CALL COMM2SD
@@ -129,18 +130,28 @@ SDRDSN2		IN A,(GSCOM)
 		POP BC
 		POP DE
 		RET
-	display "GS_INIT=",$
 GS_INIT
         ;jr SD_NO ;savelij13: please fix
 ;ПРОВЕРКА НАЛИЧИЯ КАРТОЧКИ
-		;XOR A
-		;OUT (GSDAT),A
-		;LD A,0X1D
-		;OUT (GSCOM),A
+;GSDCMP		;LD H,CMOS_BYTE_01
+		;ROMCALL READCMOS,ROM_RST82
+		;LD A,L
+		;AND M_ACCESSSDG			;ПРОВЕРКА РАЗРЕШЕНИЯ ДОСТУПА К SD КАРТЕ NEOGS
+		;JR Z,SD_NO			;ВЫХОД ЕСЛИ ДОСТУП К КАРТОЧКЕ ЗАПРЕЩЕН
+		;LD A,(FLAGS_DRV)
+		;AND B_INST_SDG
+		;JR Z,SD_NO			;ВЫХОД ЕСЛИ ДРАЙВЕР НЕ УСТАНОВЛЕН
+		XOR A
+		OUT (GSDAT),A
+		LD A,0X1D ;??? нет такой команды
+                 ld a,0x23 ;Get number of RAM Pages
+		OUT (GSCOM),A
 		;IN A,(GSCOM)
 		;RRA
 		;JR C,$-3
-		;IN A,(GSDAT)
+                 halt
+                 halt
+		IN A,(GSDAT)
 		;LD D,A
 		;AND 0X0F
 		;LD E,A
@@ -152,9 +163,10 @@ GS_INIT
 		;RRCA
 		;CP E
 		;LD A,1
-		
-		;JR NZ,GSDINIT1 
-		;надо раскоментировать, а инсталлятор перенести в main
+		;JR NZ,GSDINIT1
+                 inc a
+                 cp 3+1
+                 jr c,SD_NO ;не может быть <3 pages or 0xff pages
 		CALL INSTSDD
 
 ;ИНИЦИАЛИЗАЦИЯ КАРТОЧКИ
@@ -223,13 +235,6 @@ INSTSDD		LD A,0X80
 		;EI
 		HALT
 		;DI
-		in a,(GSSTAT)
-		and 0x01
-		jr z,gs_present
-		pop hl
-		ld a,1	;nGS'а нету
-		ret
-gs_present
 		LD A,0XF3
 		OUT (GSCOM),A
 		LD B,0X30
