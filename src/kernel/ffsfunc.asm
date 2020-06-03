40810b113e28d9ae3bbe6414420e53211d54e885
baho <baho@users.noreply.github.com>
2020-06-03 16:22:10 +0000

    * basic, browser, cmd, emptyapp, modplay, nv, ping, pkunzip, player, reset, scratch, setfont, tar, telnet, texted, unrar, view, zxrar: Minor changes to Makefiles, added useful information; * fatfs4os/Makefile: Added useful information, more compatible to Windows NT; - pkunzip/file.asm: Deleted

--- src/kernel/ffsfunc.asm
+++ src/kernel/ffsfunc.asm
@@ -1,23 +1,26 @@
-;FatFS calls 
-ffsfunc 
-.f_chdir EQU 0x67BB 
-.f_chdrive EQU 0x67B7 
-.f_chmod EQU 0x7545 
-.f_close EQU 0x67A0 
-.f_getcwd EQU 0x6816 
-.f_getfree EQU 0x6E99 
-.f_getutime EQU 0x7605 
-.f_lseek EQU 0x698F 
-.f_mkdir EQU 0x7327 
-.f_mount EQU 0x5DD5 
-.f_open EQU 0x5DFB 
-.f_opendir EQU 0x6D2F 
-.f_read EQU 0x602F 
-.f_readdir EQU 0x6DBD 
-.f_rename EQU 0x766C 
-.f_stat EQU 0x6E4F 
-.f_sync EQU 0x66D4 
-.f_truncate EQU 0x70B3 
-.f_unlink EQU 0x7208 
-.f_utime EQU 0x75A6 
-.f_write EQU 0x6358 
+; ffsfunc.asm - FatFS calls.
+; This file is a part of NedoOS project.
+; Compatible compiler: sjasmplus.
+; This file was generated automatically while building FatFS.
+ffsfunc
+.f_chdir EQU 0x67BB
+.f_chdrive EQU 0x67B7
+.f_chmod EQU 0x7545
+.f_close EQU 0x67A0
+.f_getcwd EQU 0x6816
+.f_getfree EQU 0x6E99
+.f_getutime EQU 0x7605
+.f_lseek EQU 0x698F
+.f_mkdir EQU 0x7327
+.f_mount EQU 0x5DD5
+.f_open EQU 0x5DFB
+.f_opendir EQU 0x6D2F
+.f_read EQU 0x602F
+.f_readdir EQU 0x6DBD
+.f_rename EQU 0x766C
+.f_stat EQU 0x6E4F
+.f_sync EQU 0x66D4
+.f_truncate EQU 0x70B3
+.f_unlink EQU 0x7208
+.f_utime EQU 0x75A6
+.f_write EQU 0x6358
