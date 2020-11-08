1320bfa2fdd51062e5c0fa472d08c9697a8c6a44
DimkaM <dimkam@users.noreply.github.com>
2020-11-08 13:53:40 +0000

    wget исправлен запуск плеера

--- src/kernel/build.bat
+++ src/kernel/build.bat
@@ -14,6 +14,6 @@ if "%currentdir%"=="" (
  copy /Y nedoos.$c "../../release/sd_boot.$c" > nul
  "../../tools/dmimg.exe" ../../us/sd_nedo.vhd put nedoos.$c /sd_boot.$c
  rem pause
- if "%makeall%"=="" emulatm2.bat
- rem if "%makeall%"=="" ..\..\us\emul.exe
+ rem if "%makeall%"=="" emulatm2.bat
+ if "%makeall%"=="" ..\..\us\emul.exe
 )
