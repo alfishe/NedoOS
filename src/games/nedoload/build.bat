6398d5a261ad145780f0612ac0312034073b1277
Alone <alone@users.noreply.github.com>
2021-01-30 15:00:19 +0000

    -nedoload: не хватало пары файлов и были лишние

--- src/games/nedoload/build.bat
+++ src/games/nedoload/build.bat
@@ -7,9 +7,6 @@ if not exist nedoload mkdir nedoload
 sjasmplus --nologo --msg=war --msg=war nedoload.asm
 java -jar exp2hConverter.jar nedoload.exp
 copy functions.h nedoload.h
-java -jar exp2hConverter.jar lib_tiles.exp
-copy functions.h lib_tiles.h
-rem java -jar exp2hConverter.jar functions
 del functions.h
 
 rem call compile_nedoos.bat
