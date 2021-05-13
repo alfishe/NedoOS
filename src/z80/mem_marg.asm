f6f45df3147a24abc6da9a1036c1274fbd1fe6ba
Alone <alone@users.noreply.github.com>
2021-05-13 15:16:50 +0000

    *z80: ускорена память выше 0x8000 за счёт незначительного (1%) замедления памяти ниже 0x0000..0x7fff

--- src/z80/mem_marg.asm
+++ src/z80/mem_marg.asm
@@ -19,17 +19,19 @@
         call z,next_incd
         ENDM 
 
-;TODO как определить, что мы реально щёлкали 4000, иначе проскочить?
+;TODO вариант с call nc после большинства команд, работающих с памятью
         MACRO OUTcom ;если вместо стр.команд включили др.стр.
-        LD HL,(curquart)
-        LD A,(HL)
-        OUTPGCOM
+        ;LD HL,(curquart)
+        ;LD A,(HL)
+        ;OUTPGCOM
+        call set4000com ;проскакивает, если 4000 не щёлкали
         ENDM 
 
         MACRO CALCpgcom ;изменилась конфигурация памяти, надо включить страницу для DE(PC)
-        LD HL,(curquart)
-        LD A,(HL)
-        OUTPGCOM
+        ;LD HL,(curquart)
+        ;LD A,(HL)
+        ;OUTPGCOM
+        call set4000com ;проскакивает, если 4000 не щёлкали
         ENDM 
 
      ;перед JR/CALL/RST (на входе - урезанный DE(PC), а надо получить полный)
@@ -42,23 +44,31 @@
         ENDM 
 
      ;после JR/JP/CALL/RST/RET (на входе - полный DE(PC), а надо получить урезанный)
+     ;в случае CALL/RST/RET могла подмениться 4000
         MACRO CALCiypgcom
         ld a,d
         rlca
         rlca
         dec a
         and 3 ;a=0,1,2,3 for 4000,8000,c000,0000
+        res 7,d
+        set 6,d
        ;ld hl,(curquart)
        ;cp l
        ;jp z,1f ;не получится после щёлканья в 4000
-        ld l,a
         ld (curquart),a
+        ld l,a
         ld h,currom/256
         LD A,(HL)
         OUTPGCOM
+        ld a,0xc9
+        ld (set4000com),a
+        ;ld a,0x3e
+        ;ld (setrom),a
+        ;ld (getde0000ret),a
+        ;ld (set4000),a
+        ;ld (set4000forwrite),a
 1;oldpg
-        res 7,d
-        set 6,d
         ENDM 
 
 ;портит HL,BC!
@@ -79,6 +89,9 @@
         ld a,h
         add a,a
         call nc,setmem00004000forwrite
+       if extpg5
+        call c,setmem8000c000forwrite
+       endif
         LD (HL),c
         ENDM 
 
@@ -90,8 +103,8 @@
         LD C,(HL)
         INC L
         JP NZ,1f;q ;внутри mem нет метки 1
-       rra
-       ld h,a
+        rra
+        ld h,a
         INC h
         mem
 1;q
@@ -103,15 +116,27 @@
         ld a,h
         add a,a
         call nc,setmem00004000forwrite
+       if extpg5
+        call c,setmem8000c000forwrite
+       endif
         LD (HL),c
+       if extpg5
+        rra
+        ld h,a
+        inc hl
+       else
         INC L
         JP NZ,1f;q
-       rra
-       ld h,a
+        rra
+        ld h,a
         INC h
+       endif
         ld a,h
         add a,a
         call nc,setmem00004000forwrite
+       if extpg5
+        call c,setmem8000c000forwrite
+       endif
 1;q
         LD (HL),b
         ENDM 
