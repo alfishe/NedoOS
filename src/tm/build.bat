0261cb431e250cd8588d76f991452ff2e97894a4
Kulich <kulich@users.noreply.github.com>
2021-11-06 08:12:29 +0000

    +tm: удаляем obj после сборки

--- src/tm/build.bat
+++ src/tm/build.bat
@@ -2,4 +2,6 @@
 "../../tools\mingw\make.exe"
 "../../tools/dmimg.exe" ../../us/sd_nedo.vhd put tm.com /bin/tm.com
 copy /Y tm.com ..\..\release\bin\tm.com
+
+rd /Q /S obj
 if "%makeall%"=="" ..\..\us\emul.exe
