9617c94902fa674e663545ba8e2d37ae90a41383
Alone <alone@users.noreply.github.com>
2021-12-21 21:05:40 +0000

    +noise: добавлено плавное появление и забеление в ATM3 палитре

--- src/demos/noise/noise.asm
+++ src/demos/noise/noise.asm
@@ -7,6 +7,7 @@ scrbase=0x8000
 
 music=0x4000
 
+
         org PROGSTART
 begin
         ld sp,STACK
@@ -58,8 +59,51 @@ begin
         pop bc
         push bc
         ld hl,32
-        ld de,palbuf16c
+        ld de,palbufATM3
         OS_READHANDLE
+        
+        ld hl,palbufATM3
+        ld de,palbufATM3RGB
+        ld b,16
+mkpalATM3RGB
+        push bc
+        ld a,(hl)
+        inc hl
+        push hl
+        ld h,(hl)
+        ld l,a
+        push de
+        
+        call calchexcolor ;hl=color (DDp palette) ;out: ;b=B, d=R, e=G
+
+        ld a,e ;G
+        add a,a
+        add a,a
+        add a,a
+        add a,a
+        or d ;R
+        pop de
+        ld c,a ;GR
+        ld a,b
+        add a,a
+        add a,a
+        add a,a
+        add a,a
+        ld (de),a ;B0
+        inc de
+        ld a,c
+        ld (de),a ;GR
+        inc de
+        pop hl
+        inc hl
+        pop bc
+        djnz mkpalATM3RGB
+
+        ld hl,palbufATM3RGB
+        ld de,palbufATM3
+        ld lx,8 ;lx=brightness
+        CALL recolour
+        
         pop bc
         OS_CLOSEHANDLE
         ld de,texfilename
@@ -92,7 +136,7 @@ getttexpgs0
         OS_CLOSEHANDLE
 
         ld hl,0x4000+0x1f40
-        ld de,palbuf
+        ld de,palbufATM2
         ld b,16
 copypal0
         ld a,(hl)
@@ -126,7 +170,7 @@ showpic_mainloop
 showpic0
         call setpgs_scr0       
         call showpic
-        ld de,palbuf
+        ld de,palbufATM2
         OS_SETPAL
         call halt_testquit
         ld e,0
@@ -145,10 +189,13 @@ showpic0
         jr c,showpic0
         call settimer
 
+        ld de,blackpal
+        OS_SETPAL
+
         call setpgs_scr0       
         call showpic16c
-        ld de,palbuf16c
-        OS_SETPAL
+        ;ld de,palbufATM3
+        ;OS_SETPAL
         call halt_testquit
         ld e,0
         OS_SETSCREEN
@@ -159,15 +206,61 @@ showpic0
         ld e,1
         OS_SETSCREEN
 
+        ld lx,0
+showpic1_show
+        inc lx
+        ld hl,palbufATM3RGB
+        ld de,palbufATM3
+        call recolour
+        push ix
+        ld de,palbufATM3
+        OS_SETPAL
+        halt
+        halt
+        halt
+        halt
+        halt
+        halt
+        halt
+        halt
+        pop ix
+        ld a,lx
+        cp 8
+        jr nz,showpic1_show
+
 showpic1
         call gettimer        
-        ld bc,MUSICPATTERNSIZE
+        ld bc,MUSICPATTERNSIZE;-(8*8)
         or a
         sbc hl,bc
         jr c,showpic1
+
+        ld lx,8
+showpic1_hide
+        ld hl,palbufATM3RGB
+        ld de,palbufATM3
+        call recolour
+        push ix
+        ld de,palbufATM3
+        OS_SETPAL
+        halt
+        halt
+        halt
+        halt
+        halt
+        halt
+        halt
+        halt
+        pop ix
+        inc lx
+        ld a,lx
+        cp 16
+        jr nz,showpic1_hide
+
+        ld hl,0
         call settimer
         
-        jr showpic_mainloop
+        jp showpic_mainloop
 quit
         call swapimer
 
@@ -339,14 +432,166 @@ ldirpg_a
         pop hl
         ret
 
+recolour
+;hl=palfrom (RGB)
+;de=palto (DDp)
+;lx=brightness=0..15
+        di
+        ld (recoloursp),sp
+        ld sp,hl
+       ld h,tbright/256 ;once
+        ld hx,16
+bripalATM3
+         ;ld a,(hl) ;B0
+         ;inc hl
+         ;push hl
+         ;ld b,(hl) ;GR
+       pop bc
+       ld a,c
+         ;ld h,tbright/256
+;de=palto
+;h=tbright/256
+;lx=brightness
+;a,b = B0,GR
+         add a,lx
+        ld l,a
+        ld c,(hl) ;B colour component with brightness
+        ld a,b
+        and 0xf0
+         add a,lx
+        ld l,a
+        ld a,b
+        ld b,(hl) ;G colour component with brightness
+        add a,a
+        add a,a
+        add a,a
+        add a,a
+         add a,lx
+        ld l,a
+        ld l,(hl) ;R colour component with brightness
+
+       ld a,b ;G
+       rlca ;g??G???? ;G10
+       xor l ;R
+       and 0b10010000;0b01000010 ;R10
+       xor l;gr?G??R?
+       rlca ;r?G??R?g
+       xor c ;B
+       and 0b10100101;0b01000010 ;B10
+       xor c;rbG??RBg
+       rrca ;grbG??RB
+        or 0b00001100 ;unused bits
+        ld (de),a ;low %grbG11RB
+        inc de ;TODO ld (),a
+
+       ld a,b ;G
+       rlca ;?g??G??? ;G32
+       xor l ;R
+       and 0b01001000;0b00100001 ;R32
+       xor l;?gr?G??R
+       rlca ;gr?G??R?
+       xor c ;B
+       and 0b11010010;0b00100001 ;B32
+       xor c;grbG??RB
+        or 0b00001100 ;unused bits
+        ld (de),a ;high %grbG11RB
+        inc de ;TODO ld (),a
+
+         ;pop hl
+         ;inc hl
+         dec hx
+         jp nz,bripalATM3 ;TODO dup..edup
+recoloursp=$+1
+        ld sp,0
+        ei
+        ret
+
+calchexcolor ;hl=color (DDp palette) ;out: ;b=B, d=R, e=G
+;keep c!!!
+;DDp palette: %grbG11RB(low),%grbG11RB(high), инверсные
+;high B, high b, low B, low b
+        ld b,0;0xff
+        ld de,0;0xffff
+        ld a,h
+       cpl
+        rra
+        rl b ;B high
+        rra
+        rl d ;R high
+        rra
+        rra
+        rra
+        rl e ;G high
+        rra
+        rl b ;b high
+        rra
+        rl d ;r high
+        rra
+        rl e ;g high
+        ld a,l
+       cpl
+        rra
+        rl b ;B low
+        rra
+        rl d ;R low
+        rra
+        rra
+        rra
+        rl e ;G low
+        rra
+        rl b ;b low
+        rra
+        rl d ;r low
+        rra
+        rl e ;g low
+;b=B
+;d=R
+;e=G
+        ret
+
+calcRGBtopal ;e=B, d=G, l=R ;out: b(high),c=%grbG11RB
+        call calcRGBtopal_pp
+        ld b,a
+        call calcRGBtopal_pp
+        ld c,a
+        ret
+
+calcRGBtopal_pp
+;e=B, d=G, l=R
+;DDp palette: %grbG11RB(low),%grbG11RB(high), инверсные
+        xor a
+        rl e  ;B
+        rra
+        rl l  ;R
+        rra
+        rrca
+        rrca
+        rl d  ;G
+        rra
+        rl e  ;b
+        rra
+        rl l  ;r
+        rra
+        rl d  ;g
+        rra
+        cpl
+        ret
+
 texfilename
         db "noise/forest.dat",0
 bmpfilename
         db "noise/forest.16c",0
 
-palbuf
+blackpal
+        ds 32,0xff
+
+palbufATM2
         ds 32
-palbuf16c
+
+palbufATM3RGB
+        ds 32
+
+palbufATM3
         ds 32
 
 ;timer
@@ -355,6 +600,50 @@ palbuf16c
         align 256
 ttexpgs
         ds 32
+
+
+        macro BRIGHTBYTE x
+_=x
+        if _>15
+_=15
+        endif
+_g0=~_&1
+_g1=(~_>>1)&1
+_g2=(~_>>2)&1
+_g3=(~_>>3)&1
+;0g20G3G3
+        ;db _
+        db (_g0<<6)+(_g2<<5)+(_g1<<3)+(_g3<<2)+(_g1<<1)+(_g3<<0)
+        endm
+
+        align 256
+tbright
+;brightness levels (black 0..7 less than original, 8 equal, 9..15 to white)
+;0, 1/12, 1/8, 3/16, 2/8, 3/8, 4/8, 6/8, 1
+_lev=0
+        dup 16 ;colour component level
+_antilev=15-_lev
+        BRIGHTBYTE _lev*0
+        BRIGHTBYTE _lev/12
+        BRIGHTBYTE _lev*1/8
+        BRIGHTBYTE _lev*3/16
+        BRIGHTBYTE _lev*2/8
+        BRIGHTBYTE _lev*3/8
+        BRIGHTBYTE _lev*4/8
+        BRIGHTBYTE _lev*6/8
+
+        BRIGHTBYTE _lev
+
+        BRIGHTBYTE 15-(_antilev*6/8)
+        BRIGHTBYTE 15-(_antilev*4/8)
+        BRIGHTBYTE 15-(_antilev*3/8)
+        BRIGHTBYTE 15-(_antilev*2/8)
+        BRIGHTBYTE 15-(_antilev*3/16)
+        BRIGHTBYTE 15-(_antilev*1/8)
+        BRIGHTBYTE 15
+
+_lev=_lev+1
+        edup
 
 wasmusic
         disp music
@@ -371,3 +660,5 @@ end
 	display "Size ",/d,end-begin," bytes"
 	
 	savebin "noise.com",begin,end-begin
+
+	LABELSLIST "../../../us/user.l"
