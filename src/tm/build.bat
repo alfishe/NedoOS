b2182d8428bd4c715b7fee52fd4e50c3029c506c
Kulich <kulich@users.noreply.github.com>
2022-04-13 12:19:30 +0000

    -tm  убрал tm из старого расположения

--- src/tm/build.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-"../../tools\mingw\make.exe"
-"../../tools/dmimg.exe" ../../us/sd_nedo.vhd put tm.com /bin/tm.com
-copy /Y tm.com ..\..\release\bin\tm.com
-rd /Q /S obj
-rem if "%makeall%"=="" ..\..\us\emul.exe
