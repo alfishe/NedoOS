d4cb0ce5474ee979787204caf7d98172d7366cc3
DimkaM <dimkam@users.noreply.github.com>
2022-07-17 09:26:32 +0000

    r1488: (no commit message)

--- src/_sdk/syssets.asm
+++
@@ -1,9 +0,0 @@
-atm=1 
-atm2clock=0 
-sys_npages=192 
-NEMOIDE=1 
-SYSDRV=12 
-INETDRV=0x01 
-PS2KBD=0x01 
- define NGSSD 
- define ATMRESIDENT 
--- src/browser/build.bat
+++ src/browser/build.bat
@@ -4,13 +4,13 @@ sjasmplus --nologo --msg=war browser.asm
 SET releasedir2=../../release/
 if "%currentdir%"=="" (
   FOR %%j IN (*.com) DO (
-  echo "../../tools/dmimg.exe" ../../us/sd_nedo.vhd put "%%j" "/bin/%%j"
+  rem echo "../../tools/dmimg.exe" ../../us/sd_nedo.vhd put "%%j" "/bin/%%j"
   "../../tools/dmimg.exe" ../../us/sd_nedo.vhd put "%%j" "/bin/%%j"
   move "*.com" "%releasedir2%bin" > nul
   IF EXIST %%~nj xcopy /Y "%%~nj" "%releasedir2%bin\%%~nj\" > nul
   )
+ "../../tools/dmimg.exe" ../../us/sd_nedo.vhd put browser/nos.htm /bin/browser/nos.htm
 cd ../../src/
-call ..\tools\chkimg.bat sd
 rem pause
 rem  if "%makeall%"=="" ..\..\..\us\emul.exe
  if "%makeall%"=="" ..\us\emul.exe
