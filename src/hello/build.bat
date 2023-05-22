66d4dadb3d89c952b80b9bf9642a787a510fd2c8
galstaff <galstaff@users.noreply.github.com>
2023-05-22 06:26:01 +0000

    Откатил случайно закоммиченный мусор.

--- src/hello/build.bat
+++ src/hello/build.bat
@@ -1,3 +1,12 @@
 if "%settedpath%"=="" call ../_sdk/setpath.bat
 sjasmplus --nologo --msg=war hello.asm
 rem sjasmplus --nologo --msg=war hello.asm --raw=hello.com
+
+if "%currentdir%"=="" (
+ FOR %%j IN (*.com) DO (
+ copy /Y %%j "../../release/bin/" > nul
+ "../../tools/dmimg.exe" ../../us/sd_nedo.vhd put %%j /bin/%%j
+ )
+ rem pause
+ if "%makeall%"=="" ..\..\us\emul.exe
+)
--- src/hello/hello.asm
+++ src/hello/hello.asm
@@ -3,89 +3,23 @@
 
         org PROGSTART
 begin
-	ld sp,0x4000
+        ;ld ix,(1<<9)+(2<<5)+12
+        ;ld hl,(7<<11)+(34<<5)+(23/2) ;ix=date, hl=time
+        ;OS_SETTIME
+
+        ;ld e,6 ;textmode
+        ;OS_SETGFX
         call initstdio
 
-	ld hl,okstr
-	call prtext
-
-	call ismoonsoundpresent
-
-	ld hl,okstr
-	call prtext
-
-
-	call opl4_init
-
-	ld hl,okstr
-	call prtext
-
-	ld bc,9
-	ld d,0
-	ld hl,0x1200
-	ld ix,step_buffer
-	call opl4_readmemory
-
-	ld hl,step_buffer
-	call prtext
-
-	ld de,infnamestr
-	call memorybufferloadfile
-	jr z,.loadok
-        QUIT
-
-.loadok
-	ld hl,okstr
-	call prtext
-
-	call memorybufferstart
-
-	ld hl,(memorybuffersize)
-	ld a,(memorybuffersize+2)
-	ld d,a
-	exx 
-	ld hl,0
-	ld d,0x20
-	call opl4_loadsample
-
-	ld hl,okstr
-	call prtext
-
-	call memorybufferfree
-
-;	call opl4_init
-
-	ld de,outfilenamestr
-	call openstream_file
-
-	ld hl,0
-	ld d,0x20
-	ld b,19
-.saveloop
-	push bc
-	push de
-	push hl
-
-	ld bc,16384
-	ld ix,0xc000
-	call opl4_readmemory
-
-	ld de,0xc000
-	ld hl,0x4000
-        ld a,(filehandle)
-	ld b,a
-	OS_WRITEHANDLE
-
-	pop hl
-	ld bc,16384
-	add hl,bc
-	pop de
-	jr nc,$+3
-	inc d
-	pop bc
-	djnz .saveloop
-
-	call closestream_file        
+        ld hl,thello
+        call prtext
+        ld hl,thello
+        call prtext
+        ld hl,thello
+        call prtext
+        ld hl,thello
+        call prtext
+        
         QUIT
 
 prtext
@@ -107,20 +41,11 @@ strlen
         sbc hl,bc
         ret
 
-okstr
-	db "\r\nBEBE",0
-infnamestr
-	db "timesup.mdr",0
-outfilenamestr
-	db "test.out",0
-step_buffer
-	db "                             ",0
-
+thello
+        db "Hello, world!",0x0d,0x0a,0
+        ;db "Sele",0x1b,'[','1','D',"ct drive:\r\n[0] Nemo master\r\n[1] Nemo slave\r\n[2] ATM master(not tested!)\r\n[3] ATM slave(not tested!)",0
+        
         include "../_sdk/stdio.asm"
-	include "../_sdk/file.asm"
-	include "memorybuffer.asm"
-	include "moonsound.asm"
-	include "opl4.asm"
 end
 	savebin "hello.com",begin,end-begin
 
