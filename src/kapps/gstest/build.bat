1ad6642ae184ec18a55422b42721bf83ca34d3ed
Kulich <kulich@users.noreply.github.com>
2023-07-05 04:22:44 +0000

    +gstest.com  - перенесен из kapps в bin

--- src/kapps/gstest/build.bat
+++ src/kapps/gstest/build.bat
@@ -1,5 +1,5 @@
 "../../../tools/mingw/make.exe" -f makefile %1
-"../../../tools/dmimg.exe" ../../../us/sd_nedo.vhd put gstest.com /bin/kapps/gstest.com
+"../../../tools/dmimg.exe" ../../../us/sd_nedo.vhd put gstest.com /bin/gstest.com
 rd /Q /S obj
 rem del gstest.com
 if "%makeall%"=="" ..\..\..\us\emul.exe
