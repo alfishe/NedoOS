ce75ae4ba3db964795e14bb2b67f2aa9069e6699
lvd <lvd@users.noreply.github.com>
2023-09-11 14:30:42 +0000

    +crc: added help (-h, --help), multiple files capability added

--- src/crc/main.asm
+++ src/crc/main.asm
@@ -11,6 +11,7 @@ cmd_begin
         ld sp,STACK
         call initstdio
 
+	; precalculate CRC table
         xor a
         LD L,A
 MKTCRC0 EXX 
@@ -54,21 +55,114 @@ MKTCRCe DJNZ MKTCRC1
         INC L
         INC A
         JR NZ,MKTCRC0 
-        
-        ld hl,COMMANDLINE
-        call skipword
-        call skipspaces
-        ld a,(hl)
-        or a
-        jr z,openerror
-        ;jr nz,$+5
-        ; ld hl,defaultfilename
-        ld (curfilenameaddr),hl
-        ex de,hl
+
+
+		;get name of the prog as it was called from shell
+		ld	hl,COMMANDLINE
+	
+		call	skipspaces
+		or	a
+		jp	z,fatal_err	;can't find first non-' ' char -- fatal error
+
+		ld	[name_ptr],hl	;program name
+
+		call	skipword
+		or	a
+		jp	z,got_no_args
+		ld	[hl],0		;zero-terminate program name
+		inc	hl
+		call	skipspaces
+		or	a
+		jp	z,got_no_args
+
+		;now HL points to first real arg, start arg parsing loop
+parse_args:
+		ld	[curr_arg],hl
+		;find end of arg and zero-terminate
+		call	skipword
+		or	a
+		push	af
+		push	hl
+		ld	[hl],0
+
+		call	process_arg
+
+		pop	hl
+		pop	af
+		jr	z,successful_exit
+		inc	hl
+		call	skipspaces
+		or	a
+		jr	nz,parse_args
+successful_exit:
+		ld	hl,0
+		QUIT
+
+
+AST_DFLT	equ	0
+
+process_arg:	;args parsing routine, has state
+		;in: HL=asciiz of current argument
+
+		ld	a,[argp_state]
+		or	a
+		jr	nz,.no_dflt
+
+;DFLT - wait for switches
+
+		; check for -h or --help
+		ld	hl,[curr_arg]
+		push	hl
+		ld	de,help_arg1
+		call	strcmp
+		jr	z,.arg_help
+		pop	hl
+		ld	de,help_arg2
+		call	strcmp
+		jr	z,.arg_help
+
+		;if not -h or --help -- file to CRC
+		;(preliminary, until -c mode is implemented)
+
+		ld	hl,[curr_arg]
+		call	process_file
+		
+
+		ret
+
+
+.no_dflt
+		jp	error_exit
+
+
+
+.arg_help	;print help, exit
+		ld	hl,help_msg1
+		call	prtext
+		ld	hl,[name_ptr]
+		call	prtext
+		ld	hl,help_msg2
+		call	prtext
+		jr	successful_exit
+
+
+
+
+
+
+
+process_file:
+
+	ld	de,[curr_arg]
         call openstream_file
         or	a
-        jr	nz,openerror
-readloop0
+        jr	nz,.file_error
+
+		ld	hl,0xFFFF
+		ld	[CRCArea+0],hl
+		ld	[CRCArea+2],hl
+
+.readloop0
         ld de,DISKBUF
         ld hl,DISKBUFsz
 ;de=buf
@@ -76,7 +170,7 @@ readloop0
         call readstream_file
         ld a,h
         or l
-        jr z,closequit
+        jr z,.closequit
         ld b,h
         ld c,l
 	
@@ -91,14 +185,16 @@ readloop0
         LD	(CRCArea),BC
         LD	(CRCArea+2),DE 
 
-        jr readloop0
-closequit
+        jr	.readloop0
+.closequit
         call closestream_file
-curfilenameaddr equ $+1
-        ld hl,txtcrc
-        call prtext
-        ld	hl,txtdblspc
-        call	prtext
+
+		ld	hl,[curr_arg]
+		call	prtext
+
+	        ld	hl,txtdblspc
+        	call	prtext
+
         ld hl,CRCArea
         ld a,(hl)
         cpl
@@ -120,12 +216,86 @@ curfilenameaddr equ $+1
         call prhexbyte
         call prhexbyte        
         ld hl,txtcrlf
-        call prtext
-        
-openerror
-error
-quit
-        QUIT
+;;;;;;;;call prtext
+		jr	.prtext
+
+.file_error
+		ld	hl,[curr_arg]
+		call	prtext
+		ld	hl,file_error
+.prtext
+		jp	prtext
+
+
+
+fatal_err:		; fatal error so that we can't print even error message
+		ld	hl,2
+        	QUIT
+
+
+
+got_no_args:	; no args given, print short help
+	
+		ld	hl,[name_ptr]
+		push	hl
+		call	prtext
+		ld	hl,noargs_msg1
+		call	prtext
+		pop	hl
+		call	prtext
+		ld	hl,noargs_msg2
+		call	prtext
+error_exit:
+		ld	hl,1
+		QUIT
+
+
+noargs_msg1:	db	": no args given",13,10
+		db	"Try '",0
+noargs_msg2:	db	" -h' for more information",13,10,0
+
+help_msg1:	db	"Usage: ",0
+help_msg2:	db	" [OPTION] [FILE]...",13,10
+		db	"Print CRC-32 (0xEDB88320) checksums.",13,10,13,10
+;		db	"With no FILE or when file is -, read standard input.",13,10,13,10
+;		db	"  -c, --check     read CRCs from the FILEs and check them",13,10
+		db	"  -h, --help      display this help and exit",13,10
+		db	13,10
+		db	0
+
+
+help_arg1:	db	"-h",0
+help_arg2:	db	"--help",0
+
+txtdblspc:	db	"  ",0
+txtcrlf:        db	13,10,0
+
+file_error:	db	": Error opening or reading file",13,10,0
+
+
+strcmp:		;compare strings pointed by HL and DE, case-sensitive.
+		;in: hl, de -- ptr to asciiz,
+		;out: Z,NC if equal, NZ,NC if str(DE)>str(HL), NZ,C if str(DE)<str(HL)
+		;kills: af,de,hl
+		;
+		ld	a,[hl]
+		or	a
+		ld	a,[de]
+		jr	z,.lastcmp
+		or	a
+		jr	z,.lastcmp
+		
+		cp	[hl]
+		inc	hl
+		inc	de
+		jr	z,strcmp
+		ret
+.lastcmp
+		cp	[hl]
+		ret
+
+
+
 
 skipword
 ;hl=string
@@ -179,15 +349,11 @@ prtext
         inc hl
         jr prtext
         
-txtdblspc
-	db	"  ",0
-txtcrc
-        db "CRC32=",0
-txtcrlf
-        db 13,10,0
 
 
-
+name_ptr:	dw	0
+curr_arg:	dw	0
+argp_state:	db	0
 
 
 	;bc - size
