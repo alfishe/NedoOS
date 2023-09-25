f9584ac58af3667c5374e5601ba2a3632be34a7d
Kulich <kulich@users.noreply.github.com>
2023-09-25 20:50:53 +0000

    +cmd.com - в команду pause  при старте добавлен  вызов getkey чтобы  уменьшить дребезг

--- src/cmd/cmd.asm
+++ src/cmd/cmd.asm
@@ -467,19 +467,7 @@ strcpexec_tryrun
         jr nz,execcmd_tryrunerror
 execcmd_tryrunok
         ret c ;cy=end of .bat
-        if 1==1
-       push de
-        ld b,e ;id
-        ld a,(stdinhandle)
-        ld e,a
-        ld a,(stdouthandle)
-        ld d,a
-        ld h,0xff ;rnd
-;b=id, e=stdin, d=stdout, h=stderr        
-        OS_SETSTDINOUT
-       pop de
-        endif
-        push de        
+        push de
         OS_RUNAPP ;e=id
         pop de
          xor a ;z
@@ -1606,6 +1594,7 @@ cmd_echo
         jp prcrlf
 		
 cmd_pause
+	call getkey
         ld hl,(execcmd_pars)
 		ld a,(hl)
         or a
