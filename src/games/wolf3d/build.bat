f8fa1c4b12253ec508dfddf9e4925bcb46c6b71d
Alone <alone@users.noreply.github.com>
2024-01-13 07:29:06 +0000

    +wolf3d: автоматически вычёркиваются лишние линии с текстуры. добавлены тестовые альтернативные паки (работают только 3,5,6, для остальных недостаточно памяти)

--- src/games/wolf3d/build.bat
+++ src/games/wolf3d/build.bat
@@ -7,7 +7,8 @@ sjasmplus --nologo --msg=war music.asm
 copy wolftex.bmp wolf3d
 copy wolfspr.bmp wolf3d
 
-"../../_sdk/nedores.exe" walls.bmp walls.dat walls.ast
+rem "../../_sdk/nedores.exe" walls.bmp walls.dat walls.ast
+"../../_sdk/nedores.exe" textures_06_64x64_.bmp walls.dat walls.ast
 "../../_sdk/nedores.exe" goods.bmp goods.dat goods.ast
 sjasmplus --nologo --msg=war walls.ast --raw=walls.bin
 sjasmplus --nologo --msg=war goods.ast --raw=goods.bin
@@ -35,5 +36,5 @@ cd ../../../src/
 call ..\tools\chkimg.bat sd
  rem pause
 rem  if "%makeall%"=="" ..\..\..\us\emul.exe
- if "%makeall%"=="" ..\us\emul.exe
+ if "%makeall%"=="" ..\us\emul.exe games\wolf3d\wolf.trd
 )
