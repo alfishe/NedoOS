b17a79eb98bff6d119ee47acf5c6fa1e128e3ec9
Kulich <kulich@users.noreply.github.com>
2024-06-24 17:51:16 +0000

    -auart.com +cuart.com  - удалена старая auart.com  в пользу новой универсальной cuart.com

--- src/kapps/auart/build.bat
+++
@@ -1,6 +0,0 @@
-rem @ECHO OFF
-"../../../tools/mingw/make.exe" -f makefile %1
-"../../../tools/dmimg.exe" ../../../us/sd_nedo.vhd  put auart.com /bin/auart.com
-rd /Q /S obj
-rem if "%makeall%"=="" ..\..\us\emul.exe
-
