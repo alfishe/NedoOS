54d940a37150bc69f99350d73cb097f8fd957814
Kulich <kulich@users.noreply.github.com>
2024-06-24 18:09:46 +0000

    +mrf.com -moon.com  - Старые версии moonua.com и moonue.com перенесены в KAPPS  - Новые версии mrfua.com и mrfue.com перенесены в BIN

--- src/moon-rabbit-zx/build.bat
+++ src/moon-rabbit-zx/build.bat
@@ -8,16 +8,16 @@ del /Q moonue.com
 ren  moon.com moonue.com
 "../../tools\mingw\make.exe" nedoosatm
 ren  moon.com moonua.com
-copy /Y moonua.com ..\..\release\bin\moonua.com
-copy /Y moonue.com ..\..\release\bin\moonue.com
+copy /Y moonua.com ..\..\release\bin\kapps\moonua.com
+copy /Y moonue.com ..\..\release\bin\kapps\moonue.com
 "../../tools\mingw\make.exe" nedoos
 copy /Y moon.com ..\..\release\bin\moon.com
 rem copy /Y data\index.gph ..\..\release\bin\browser\index.gph
 copy /Y data\logo.scr ..\..\release\bin\browser\logo.scr
 
 "../../tools/dmimg.exe" ../../us/sd_nedo.vhd put moon.com /bin/moon.com
-"../../tools/dmimg.exe" ../../us/sd_nedo.vhd put moonua.com /bin/moonua.com
-"../../tools/dmimg.exe" ../../us/sd_nedo.vhd put moonue.com /bin/moonue.com
+"../../tools/dmimg.exe" ../../us/sd_nedo.vhd put moonua.com /bin/kapps/moonua.com
+"../../tools/dmimg.exe" ../../us/sd_nedo.vhd put moonue.com /bin/kapps/moonue.com
 rem "../../tools/dmimg.exe" ../../us/sd_nedo.vhd put data/index.gph /bin/browser/index.gph
 "../../tools/dmimg.exe" ../../us/sd_nedo.vhd put data/logo.scr /bin/browser/logo.scr
 
