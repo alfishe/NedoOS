2a141f1119d1546c411eed798606c110237f3e41
Kulich <kulich@users.noreply.github.com>
2024-10-14 21:17:25 +0000

    -cuart.com  - исправлена ошибка открытия конфигурационного файла

--- src/kapps/cuart/build.bat
+++ src/kapps/cuart/build.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 "../../../tools\mingw\make.exe"
-"../../../tools/dmimg.exe" ../../../us/sd_nedo.vhd put cuart.com /bin/kapps/cuart.com
+"../../../tools/dmimg.exe" ../../../us/sd_nedo.vhd put cuart.com /bin/cuart.com
 rd /Q /S obj
 del cuart.com
 rem if "%makeall%"=="" ..\..\..\us\emul.exe
--- src/kapps/cuart/main.c
+++ src/kapps/cuart/main.c
@@ -48,12 +48,12 @@ void loadEspConfig(void)
   unsigned char curParam[256];
   unsigned char res;
   FILE *espcom;
-  OS_SETSYSDRV();
-  OS_CHDIR("browser");
+	OS_SETSYSDRV();
+	OS_CHDIR("../ini");
   espcom = OS_OPENHANDLE("espcom.ini", 0x80);
   if (((int)espcom) & 0xff)
   {
-    printf("mrfesp.ini opening error\r\n");
+    printf("espcom.ini opening error\r\n");
     return;
   }
 
