db78f9ce4904e3398d7e978e069c5009366fd069
Kulich <kulich@users.noreply.github.com>
2024-11-23 07:55:04 +0000

    +gopher.com   - при сборке кладем домашнюю страницу в образ/релиз.

--- src/kapps/gopher/build.bat
+++ src/kapps/gopher/build.bat
@@ -1,5 +1,7 @@
 "../../../tools/mingw/make.exe" -f makefile %1
 "../../../tools/dmimg.exe" ../../../us/sd_nedo.vhd put gopher.com /bin/gopher.com
+"../../../tools/dmimg.exe" ../../../us/sd_nedo.vhd put nedogoph.gph /bin/browser/nedogoph.gph
+copy /Y nedogoph.gph ..\..\..\release\bin\browser\nedogoph.gph
 rd /Q /S obj
 del gopher.com
 if "%makeall%"=="" ..\..\..\us\emul.exe
