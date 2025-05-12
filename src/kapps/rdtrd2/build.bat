784fc32d105a2f9211f6b6327d30b00ecd64fe0d
Kulich <kulich@users.noreply.github.com>
2025-05-12 22:14:14 +0000

    пересобраны бинарники со свежими библиотеками.

--- src/kapps/rdtrd2/build.bat
+++ src/kapps/rdtrd2/build.bat
@@ -4,4 +4,4 @@
 "../../../tools/dmimg.exe" ../../../us/sd_nedo.vhd put rdtrd2.com /bin/rdtrd2.com
 rd /Q /S obj
 del rdtrd2.com
-rem if "%makeall%"=="" ..\..\..\us\emul.exe
+if "%makeall%"=="" ..\..\..\us\emul.exe
--- src/kapps/wrtrd2/build.bat
+++ src/kapps/wrtrd2/build.bat
@@ -3,4 +3,4 @@
 "../../../tools/dmimg.exe" ../../../us/sd_nedo.vhd put wrtrd2.com /bin/wrtrd2.com
 rd /Q /S obj
 del wrtrd2.com
-rem if "%makeall%"=="" ..\..\..\us\emul.exe
+if "%makeall%"=="" ..\..\..\us\emul.exe
