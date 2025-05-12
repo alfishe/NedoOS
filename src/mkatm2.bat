91ab3c3058deb3ae6a2dcbc50abc7959caa44a0b
Kulich <kulich@users.noreply.github.com>
2025-05-12 20:27:34 +0000

    +build  - теперь можно собирать систему не в репе SVN, ранее такое было возможно только если у пользователя не было svnversion.exe

--- src/mkatm2.bat
+++ src/mkatm2.bat
@@ -9,7 +9,7 @@ echo PS2KBD=0x00 >> _sdk\syssets.asm
 echo  define ATMRESIDENT >> _sdk\syssets.asm
 echo  define KEEPPG38 >> _sdk\syssets.asm
 set makeall=1
-FOR /F "tokens=1 delims=:MSP " %%i IN ('svnversion -n') DO echo  define SVNREVISION %%i >> _sdk\syssets.asm
+FOR /F "tokens=1 delims=:MSPUnversiodcty " %%i IN ('svnversion -n') DO echo  define SVNREVISION %%i >> _sdk\syssets.asm
 call make.bat
 move test.trd ..\release\osatm2.trd > nul
 if "%notrunemu%"=="" ..\us\emul.exe -i atm2.ini ..\release\osatm2.trd
--- src/mkevo.bat
+++ src/mkevo.bat
@@ -9,7 +9,7 @@ echo PS2KBD=0x01 >> _sdk\syssets.asm
 echo  define NGSSD >> _sdk\syssets.asm
 echo  define ATMRESIDENT >> _sdk\syssets.asm
 set makeall=1
-FOR /F "tokens=1 delims=:MSP " %%i IN ('svnversion -n') DO echo  define SVNREVISION %%i >> _sdk\syssets.asm
+FOR /F "tokens=1 delims=:MSPUnversiodcty " %%i IN ('svnversion -n') DO echo  define SVNREVISION %%i >> _sdk\syssets.asm
 call make.bat noneedtrd
 cd  kernel
 ..\..\tools\sjasmplus --nologo --msg=war hobeta.asm > nul
