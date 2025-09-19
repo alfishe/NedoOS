51c5e7eaa6c64c748e19f73471554256c69fd67e
DimkaM <dimkam@users.noreply.github.com>
2025-09-19 18:08:10 +0000

    добавлен svn.com

---
+++ release/bin/upd.bat
@@ -0,0 +1,4 @@
+rem svn lst nedoos.ru/nedoos/nedoos/release/ -v
+rem svn log nedoos.ru/nedoos/nedoos/release/ -r 2368:2370
+svn upd nedoos.ru/nedoos/nedoos/release/bin /bin
+svn upd nedoos.ru/nedoos/nedoos/release/sd_boot.$C /
