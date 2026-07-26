5dafd791f0bc57f9c8570b2b89fc628c3e9d5966
Kulich <kulich@users.noreply.github.com>
2026-07-26 18:34:34 +0000

    отказ от playground

--- src/kapps/playground/multibank/mb_inc.h
+++
@@ -1,9 +0,0 @@
-#ifndef MB_INC_H
-#define MB_INC_H
-
-#include <string.h>
-#include <stdio.h>
-#include <intrz80.h>
-#include <oscalls.h>
-
-#endif
--- src/kapps/playground/tdesk/build.bat
+++
@@ -1,7 +0,0 @@
-"../../../../tools/mingw/make.exe" -f Makefile %1
-"../../../../tools/dmimg.exe" ../../../../us/sd_nedo.vhd put tdesk.com /bin/kapps/tdesk.com
-"../../../../tools/dmimg.exe" ../../../../us/sd_nedo.vhd put forms\welcome.frm /bin/kapps/welcome.frm
-"../../../../tools/dmimg.exe" ../../../../us/sd_nedo.vhd put forms\dialog.frm /bin/kapps/dialog.frm
-rd /Q /S obj
-rem del tdesk.com
-if "%makeall%"=="" ..\..\..\..\us\emul.exe
