817888e0853ab8d7d8b1005177fceb08b584a37a
Kulich <kulich@users.noreply.github.com>
2026-08-05 16:10:50 +0000

    вместо nc собираем под его именем nco

--- src/kapps/nc/build.bat
+++ src/kapps/nc/build.bat
@@ -1,6 +1,6 @@
-"../../../tools/mingw/make.exe" -f makefile %1
-"../../../tools/dmimg.exe" ../../../us/sd_nedo.vhd put nc.com /bin/nc.com
-"../../../tools/dmimg.exe" ../../../us/sd_nedo.vhd put nc.ext /ini/nc.ext
-if "%makeall%"=="" ..\..\..\us\emul.exe
-rd /Q /S obj
+rem "../../../tools/mingw/make.exe" -f makefile %1
+rem "../../../tools/dmimg.exe" ../../../us/sd_nedo.vhd put nc.com /bin/nc.com
+rem "../../../tools/dmimg.exe" ../../../us/sd_nedo.vhd put nc.ext /ini/nc.ext
+rem if "%makeall%"=="" ..\..\..\us\emul.exe
+rem rd /Q /S obj
 
